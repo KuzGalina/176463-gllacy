89e22a714c992200e47d4e0fc6ba01d8f237a67f
KuzGalina <son-galina@yandex.ru>
2017-05-09 13:00:04 +0600

    декоративные элементы

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+		  var link = document.querySelector(".contacts-btn");
+		  var popup = document.querySelector(".modal-content");
+		  var close = popup.querySelector(".modal-content-close");
+		  var overlay = document.querySelector(".modal-overlay");
+	
+			
+		  link.addEventListener("click", function(evt) {
+			  
+			  
+			evt.preventDefault();
+			popup.classList.add("modal-content-show");
+			overlay.classList.add("modal-content-show");
+		 });
+	
+		  close.addEventListener("click", function(evt) {		  
+			evt.preventDefault();
+			popup.classList.remove("modal-content-show");
+		  });
