1b8caa062ad43e3a263bfedade5db237cbe2999b
KuzGalina <son-galina@yandex.ru>
2017-05-13 16:19:38 +0600

    финальная проверка 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,30 @@
 		  var popup = document.querySelector(".modal-content");
 		  var close = popup.querySelector(".modal-content-close");
 		  var overlay = document.querySelector(".modal-overlay");
+		  var input = popup.querySelectorAll("input");
+		 /* var email = popup.querySelector("[name=email]");
+		  var comment = popup.querySelector("[name=comment]");
+		  var form = popup.querySelector("form");  */
+		  var label = popup.querySelector("label");
 	
 			
-		  link.addEventListener("click", function(evt) {
-			  
-			  
+		link.addEventListener("click", function(evt) {
 			evt.preventDefault();
 			popup.classList.add("modal-content-show");
 			overlay.classList.add("modal-content-show");
+			login.focus();
+		
+			
 		 });
 	
-		  close.addEventListener("click", function(evt) {		  
+	  	close.addEventListener("click", function(evt) {		  
 			evt.preventDefault();
 			popup.classList.remove("modal-content-show");
 			overlay.classList.remove("modal-content-show");
 		  });
+		  
+		input.addEventListener("click", function() {
+			label.classList.add("label-show");	
+		});
+		
+
